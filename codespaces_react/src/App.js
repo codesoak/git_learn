3733c29fcf837243ff86cb71d05619a6c3cb7f64
codesoak <138311046+codesoak@users.noreply.github.com>
2023-08-20 14:32:39 +0000

    react added in codespace

---
+++ codespaces_react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Helloo World!
+    </div>
+  );
+}
+
+export default App;
--- new.js
+++ new.js
@@ -1,2 +1,3 @@
 let num = 78
 let num2 = 89
+let num3 = 100
